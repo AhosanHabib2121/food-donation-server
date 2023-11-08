d4c88405ca1fd38d464dd880278e6156dbd847ab
ahosanhabib2121 <ahosanhabib21021@gmail.com>
2023-11-08 11:17:26 +0600

    jwt token use login and register securtiy purpose and some routes

--- index.js
+++ index.js
@@ -1,15 +1,41 @@
 const express = require('express');
 const cors = require('cors');
-const app = express();
+const jwt = require('jsonwebtoken')
+const cookieParser = require('cookie-parser');
 require('dotenv').config();
-const {MongoClient, ServerApiVersion, ObjectId} = require('mongodb');
+const {MongoClient, ServerApiVersion} = require('mongodb');
+const app = express();
 const port = process.env.PORT || 5000
 
 
 // middleware use here
-app.use(cors());
-app.use(express.json())
+app.use(cors({
+    origin: ['http://localhost:5173'],
+    credentials: true
+}));
+app.use(express.json());
+app.use(cookieParser());
 
+
+// verifyToken here
+const verifyToken = (req, res, next) => {
+    const token = req ?.cookies?.token;
+    if (!token) {
+        return res.status(401).send({
+            message: 'Unauthorized access'
+        })
+    }
+    jwt.verify(token, process.env.ACCESS_TOKEN, (err, decoded) => {
+        if (err) {
+            return res.status(401).send({
+                message: 'Unauthorized access'
+            })
+        }
+        req.user = decoded;
+        next()
+    });
+
+}
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.algulij.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -30,6 +56,33 @@ async function run() {
         const foodCollection = client.db("foodDonationDB").collection("foods");
         const foodRequestCollection = client.db("foodDonationDB").collection("foodRequest");
 
+
+         // -------------------Authentication related api------------
+         app.post('/jwt', async (req, res) => {
+             const user = req.body;
+             const token = jwt.sign(user, process.env.ACCESS_TOKEN, {
+                 expiresIn: '1h'
+             })
+             res.cookie('token', token, {
+                     httpOnly: true,
+                     secure: process.env.NODE_ENV === 'production',
+                     sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
+                 })
+                 .send({
+                     success: true
+                 })
+         })
+        // logout and clear cookies data
+        app.post('/logout', async (req, res) => {
+            const user = req.body;
+            res
+                .clearCookie('token', {
+                    maxAge: 0
+                })
+                .send({
+                    success: true
+                })
+        })
 
         // -------userCollection here---------
         app.post('/user', async (req, res) => {
@@ -52,7 +105,7 @@ async function run() {
         })
 
         // -------foodCollection here---------
-        app.post('/foods', async (req, res) => {
+        app.post('/foods', verifyToken, async (req, res) => {
             const foodsData = req.body;
             const result = await foodCollection.insertOne(foodsData);
             res.send(result);
